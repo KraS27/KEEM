7d9124a777b78df9c6649439eaf95164386772ed
KraSS <groverkras@gmail.com>
2023-04-18 19:29:12 +0300

    Add GOOGLE API KEY

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from "./Components/Navigation/Navigation";
 import Landing from "./Components/Content/Landing/Landing";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import TestMap from "./Components/Content/Maps/TestMap";
+import TestMap from "./Components/Content/Map/TestMap";
+const API_KEY = process.env.REACT_APP_API_KEY;
+console.log(API_KEY);
 
 function App() {
   return (
