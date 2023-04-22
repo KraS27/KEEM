07b13218d6fc2d74d8c4d0830f93cbc36a7671de
KraSS <groverkras@gmail.com>
2023-04-22 20:34:07 +0300

    Refactor Pois

--- src/Components/Content/Map/Map.jsx
+++ src/Components/Content/Map/Map.jsx
@@ -1,15 +1,12 @@
-import React, {useState} from 'react';
-import {GoogleMap, InfoWindow, Marker, useJsApiLoader} from "@react-google-maps/api";
+import React from 'react';
+import {GoogleMap, InfoWindow, Marker} from "@react-google-maps/api";
 import s from "./Map.module.css"
 import {Button} from "react-bootstrap";
 
 const Map = (props) => {
 
-    const [selectedMarker, setSelectedMarker] = useState(undefined);
-
     const markersMap = props.pois.map(m => <Marker position={{lat: m.latitude, lng: m.longitude}}
-                                                onClick = {() => { setSelectedMarker(m)}}
-    />)
+                                                   onClick = {() => { props.setSelectedMarker(m)}}/>)
 
     return (
         <div className={s.mapContainer}>
@@ -22,13 +19,13 @@ const Map = (props) => {
                 options={props.defaultOptions}
             >
                 {markersMap}
-                {selectedMarker && (
-                    <InfoWindow position={{lat: selectedMarker.latitude, lng: selectedMarker.longitude}}
-                                onCloseClick={() => { setSelectedMarker(null) }}
+                {props.selectedMarker && (
+                    <InfoWindow position={{lat: props.selectedMarker.latitude, lng: props.selectedMarker.longitude}}
+                                onCloseClick={() => { props.setSelectedMarker(null) }}
                                 options={{pixelOffset: new window.google.maps.Size(0, -40)}}
                     >
                         <div>
-                            <p>{selectedMarker.nameObject}</p>
+                            <p>{props.selectedMarker.nameObject}</p>
                             <Button>fasfsa</Button>
                         </div>
                     </InfoWindow>
