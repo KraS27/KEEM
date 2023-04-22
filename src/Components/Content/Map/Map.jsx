5a0991ce143c48caff40941fc402071d8de887a8
KraSS <groverkras@gmail.com>
2023-04-22 20:25:32 +0300

    Move all business logic from Map to MapContainer

--- src/Components/Content/Map/Map.jsx
+++ src/Components/Content/Map/Map.jsx
@@ -1,66 +1,25 @@
 import React, {useState} from 'react';
 import {GoogleMap, InfoWindow, Marker, useJsApiLoader} from "@react-google-maps/api";
 import s from "./Map.module.css"
-import axios from "axios";
 import {Button} from "react-bootstrap";
-const API_KEY = process.env.REACT_APP_API_KEY;
 
-const containerStyle = {
-    width: '100%',
-    height: '100%'
-};
-const defaultCenter = {
-    lat: 50.4311172066137,
-    lng: 30.6573808193207
-};
-const defaultOptions = {
-    panControl: true,
-    zoomControl: true,
-    mapTypeControl: false,
-    scaleControl: false,
-    streetViewControl: false,
-    rotateControl: false,
-    clickableIcons: false,
-    keyboardShortcuts: false,
-    disableDoubleClickZoom: true,
-}
+const Map = (props) => {
 
-let markers = [];
-
-const Map = () => {
-
-    const mapRef = React.useRef(null);
     const [selectedMarker, setSelectedMarker] = useState(undefined);
-    const onLoad = React.useCallback(function callback(map) {
-        mapRef.current = map;
-    }, []);
-    const onUnmount = React.useCallback(function callback(map) {
-        mapRef.current = undefined;
-    }, []);
-    const { isLoaded } = useJsApiLoader({
-        id: 'google-map-script',
-        googleMapsApiKey: API_KEY
-    });
 
-
-    axios.get("https://localhost:7199/pois").then(response => {
-        debugger;
-        markers = response.data.data;
-    })
-
-    const markersMap = markers.map(m => <Marker position={{lat: m.latitude, lng: m.longitude}}
+    const markersMap = props.pois.map(m => <Marker position={{lat: m.latitude, lng: m.longitude}}
                                                 onClick = {() => { setSelectedMarker(m)}}
     />)
 
-    return isLoaded ? (
+    return (
         <div className={s.mapContainer}>
             <GoogleMap
-                mapContainerStyle={containerStyle}
-                center={defaultCenter}
+                mapContainerStyle={props.containerStyle}
+                center={props.defaultCenter}
                 zoom={10}
-                onLoad={onLoad}
-                onUnmount={onUnmount}
-                options={defaultOptions}
+                onLoad={props.onLoad}
+                onUnmount={props.onUnmount}
+                options={props.defaultOptions}
             >
                 {markersMap}
                 {selectedMarker && (
@@ -76,7 +35,7 @@ const Map = () => {
                 )}
             </GoogleMap>
         </div>
-    ) : <></>
+    )
 };
 
 export default Map;
