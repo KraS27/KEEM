ab0b5dbcacce9c1442a28cf22c5f175fbcf5ed6c
KraSS <groverkras@gmail.com>
2023-04-22 20:28:19 +0300

    Move state from Map to MapContainer

---
+++ src/Components/Content/Map/MapContainer.jsx
@@ -0,0 +1,65 @@
+import React, {useState} from 'react';
+import {GoogleMap, InfoWindow, Marker, useJsApiLoader} from "@react-google-maps/api";
+import axios from "axios";
+import Map from "./Map"
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+const containerStyle = {
+    width: '100%',
+    height: '100%'
+};
+const defaultCenter = {
+    lat: 50.4311172066137,
+    lng: 30.6573808193207
+};
+const defaultOptions = {
+    panControl: true,
+    zoomControl: true,
+    mapTypeControl: false,
+    scaleControl: false,
+    streetViewControl: false,
+    rotateControl: false,
+    clickableIcons: false,
+    keyboardShortcuts: false,
+    disableDoubleClickZoom: true,
+}
+let pois = [];
+
+const MapContainer = () => {
+
+    const mapRef = React.useRef(null);
+    const [selectedMarker, setSelectedMarker] = useState(undefined);
+    const onLoad = React.useCallback(function callback(map) {
+        mapRef.current = map;
+    }, []);
+    const onUnmount = React.useCallback(function callback(map) {
+        mapRef.current = undefined;
+    }, []);
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: API_KEY
+    });
+
+    axios.get("https://localhost:7199/pois").then(response => {
+        pois = response.data.data;
+    })
+
+    if(isLoaded){
+        return(
+            <Map onLoad={onLoad}
+                 onUnmount={onUnmount}
+                 containerStyle={containerStyle}
+                 defaultCenter={defaultCenter}
+                 defaultOptions={defaultOptions}
+                 selectedMarker={selectedMarker}
+                 setSelectedMarker={setSelectedMarker}
+                 pois={pois}
+            />
+        )
+    }
+    else {
+        return (<></>)
+    }
+};
+
+export default MapContainer;
