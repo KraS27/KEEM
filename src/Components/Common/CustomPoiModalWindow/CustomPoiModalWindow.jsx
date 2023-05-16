0205e609fd75f5ab319e92749535e368c05abd81
KraSS <groverkras@gmail.com>
2023-05-16 16:03:35 +0300

    Add possibility to change custom markers

--- src/Components/Common/CustomPoiModalWindow/CustomPoiModalWindow.jsx
+++ src/Components/Common/CustomPoiModalWindow/CustomPoiModalWindow.jsx
@@ -16,13 +16,13 @@ const CustomPoiModalWindow = (props) => {
             >
                 <Modal.Body>
                     <p>Назва точки: </p>
-                    <MDBInput type="text" ></MDBInput>
+                    <MDBInput type="text" value={props.nameObject} onChange={e => props.setNameObject(e.target.value)}/>
                     <p>Розташування: </p>
-                    <MDBInput type="text" ></MDBInput>
+                    <MDBInput type="text" value={props.description} onChange={e => props.setDescription(e.target.value)}/>
                     <p>Широта: </p>
-                    <MDBInput type="text" ></MDBInput>
+                    <MDBInput type="text" value={props.latitude}/>
                     <p>Довгота: </p>
-                    <MDBInput type="text" ></MDBInput>
+                    <MDBInput type="text" value={props.longitude}/>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={props.handleClose}>
--- src/Components/Common/CustomPoiModalWindow/CustomPoiModalWindowContainer.jsx
+++ src/Components/Common/CustomPoiModalWindow/CustomPoiModalWindowContainer.jsx
@@ -3,15 +3,36 @@ import CustomPoiModalWindow from "./CustomPoiModalWindow";
 
 const CustomPoiModalWindowContainer = (props) => {
 
+    debugger;
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
+    const [nameObject, setNameObject] = useState(props.marker.nameObject);
+    const [description, setDescription] = useState(props.marker.description);
+    const [latitude, setLatitude] = useState(props.marker.latitude);
+    const [longitude, setLongitude] = useState(props.marker.longitude);
+
+    const handleSetNameObject = (newNameObject) => {
+        setNameObject(newNameObject);
+        props.marker.nameObject = nameObject;
+    }
+    const handleDescription = (newDescription) => {
+        setDescription(newDescription);
+        props.marker.description = description;
+    }
     return (
         <CustomPoiModalWindow
             show={show}
             handleClose={handleClose}
             handleShow={handleShow}
             marker={props.marker}
+            nameObject={nameObject}
+            description={description}
+            setNameObject={handleSetNameObject}
+            setDescription={handleDescription}
+            latitude={latitude}
+            longitude={longitude}
         />
     );
 };
