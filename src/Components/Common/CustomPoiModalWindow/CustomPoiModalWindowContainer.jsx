282fb53feca76823c5efa57fa1f3b08f929fb78c
KraSS <groverkras@gmail.com>
2023-05-18 17:53:10 +0300

    Create test post request to addPoi

--- src/Components/Common/CustomPoiModalWindow/CustomPoiModalWindowContainer.jsx
+++ src/Components/Common/CustomPoiModalWindow/CustomPoiModalWindowContainer.jsx
@@ -1,18 +1,39 @@
 import React, {useState} from 'react';
 import CustomPoiModalWindow from "./CustomPoiModalWindow";
+import axios from "axios";
+import {useParams} from "react-router-dom";
 
 const CustomPoiModalWindowContainer = (props) => {
 
     debugger;
     const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-
     const [nameObject, setNameObject] = useState(props.marker.nameObject);
     const [description, setDescription] = useState(props.marker.description);
     const [latitude, setLatitude] = useState(props.marker.latitude);
     const [longitude, setLongitude] = useState(props.marker.longitude);
+    const handleClose = () => {
+        axios.post(
+        "https://localhost:7199/pois",
+        {
+                 idOfUser: 2,
+                 ownerType: 4,
+                 latitude: props.marker.latitude,
+                 longitude: props.marker.longitude,
+                 description: description,
+                 nameObject: nameObject,
+                 typeName: "test",
+                 isPolluted: -1
+             },
+            {withCredentials : true}
+        ).then(response => {
+            debugger;
+            if(response.data.data === true)
+                setShow(false);
+        })
 
+
+    }
+    const handleShow = () => setShow(true);
     const handleSetNameObject = (newNameObject) => {
         setNameObject(newNameObject);
         props.marker.nameObject = nameObject;
