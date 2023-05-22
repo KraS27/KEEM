6798ff9fb7b36407e830a17175ba50cac5a1f97b
KraSS <groverkras@gmail.com>
2023-05-22 16:50:23 +0300

    Add AddEmissionModalBody and its container

---
+++ src/Components/Common/PoiModalWindow/AddEmissionModalBody.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddEmissionModalBody = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AddEmissionModalBody;
---
+++ src/Components/Common/PoiModalWindow/AddEmissionModalBodyContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import AddEmissionModalBody from "./AddEmissionModalBody";
+
+const AddEmissionModalBodyContainer = () => {
+    return (
+        <AddEmissionModalBody />
+    );
+};
+
+export default AddEmissionModalBodyContainer;
